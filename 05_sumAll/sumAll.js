ce10c58e04518904002b8bd7fa07ff565acd29b3
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-07 16:52:38 +1200

    Complete exercise05 test 03

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
 const sumAll = function(first, last) {
   let sum = 0;
 
+  if (first > last) {
+    let temp = first;
+    first = last;
+    last = temp;
+  }
+
   for (; first <= last; first += 1) {
     sum += first;
   }
