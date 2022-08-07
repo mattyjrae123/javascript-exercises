5f47487bfd9600462660875adf4645a8df8bd4f9
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-07 17:20:12 +1200

    Complete exercise07 test 01

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,5 +1,11 @@
-const ftoc = function() {
+const ftoc = function(tempFahrenheit) {
+  let tempCelcius = (tempFahrenheit-32) / (5/9);
 
+  if (tempCelcius % 1 !== 0) {
+    return tempCelcius.toFixed(1);
+  }
+
+  return tempCelcius;
 };
 
 const ctof = function() {
