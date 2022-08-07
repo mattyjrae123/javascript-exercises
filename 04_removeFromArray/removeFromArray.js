2ac97880c14d75a786da851c56d04d2e12f3392a
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-07 15:42:52 +1200

    Complete exercise04 test 01

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, itemToRemove) {
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] === itemToRemove) {
+      array.splice(i, 1);
+    }
+  }
 
+  return array;
 };
 
 // Do not edit below this line
