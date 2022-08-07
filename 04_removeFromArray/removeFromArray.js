de4d598684b94ca5f91f9f1baab9b6b54e33d45e
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-07 15:52:54 +1200

    Complete exercise04 test 02

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,13 @@
-const removeFromArray = function(array, itemToRemove) {
-  for(let i = 0; i < array.length; i++) {
-    if (array[i] === itemToRemove) {
-      array.splice(i, 1);
+const removeFromArray = function(array) {
+  // iterate over function arguments; skip first argument
+  for (let arg = 1; arg < arguments.length; arg += 1) {
+
+    // iterate over array items from first argument
+    for (let i =  0; i < array.length; i += 1) {
+
+      if (array[i] == arguments[arg]) {
+        array.splice(i, 1);
+      }
     }
   }
 
