88df1acf40e6cb3f5a27a19609d5fca5ebe2e13c
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-17 14:58:00 +1200

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // remove non-character values from string (punctuation etc)
+  string = string.split('')
+                 .filter(char => char.toLowerCase() !== char.toUpperCase())
+                 .reduce((prev, next) => prev + next)
+                 .toLowerCase();
 
+  for (let n = 0; n < string.length; n+=1) {
+    if (string[n] !== string[(string.length-1)-n]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
