93baaf2dcb91f5591a062652792bdd51eca3e31f
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-17 15:42:32 +1200

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+  let titleArr = [];
 
+  books.forEach(book => {
+    titleArr.push(book.title);
+  });
+
+  return titleArr;
 };
 
 // Do not edit below this line
