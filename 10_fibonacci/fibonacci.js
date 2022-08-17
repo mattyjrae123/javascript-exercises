6eaf70a6801a6340e584694d912f514a69a44aa7
Matthew Rae <mattyjrae123@hotmail.com>
2022-08-17 15:35:44 +1200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(targetIdx) {
+  if (targetIdx == 1 || targetIdx == 2) {
+    return 1;
+  }
 
+  if (targetIdx < 0) {
+    return "OOPS";
+  }
+
+  let prevFibValue = 1;
+  let fibValue = 2;
+  let currentIdx = 3;
+
+  while (currentIdx < targetIdx) {
+    let temp = fibValue;
+    fibValue += prevFibValue;
+    prevFibValue = temp;
+
+    currentIdx += 1;
+  }
+
+  return fibValue;
 };
 
 // Do not edit below this line
